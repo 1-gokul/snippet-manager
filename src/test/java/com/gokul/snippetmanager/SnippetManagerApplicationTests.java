49c5e9322e0d2a86323863df602dc6846ed58a1a
Gokul Prasad <gokulprasad497@gmail.com>
2026-03-13 16:31:06 +0530

    Add initial test class for SnippetManagerApplication

---
+++ src/test/java/com/gokul/snippetmanager/SnippetManagerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gokul.snippetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnippetManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
